537c7a8ccc3284d4883a00c22dea1fb4837367e1
pavanponnella <pavankumarreddy9281@gmail.com>
2024-10-03 17:57:40 +0530

    swagger congig

---
+++ src/main/java/Layered/architecture/layered/config/SwaggerClass.java
@@ -0,0 +1,15 @@
+package Layered.architecture.layered.config;
+
+
+import jdk.javadoc.doclet.Doclet;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class SwaggerClass {
+
+
+
+
+//    public Dock
+}
