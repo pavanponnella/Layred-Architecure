45a34b671ddfea7927d667d462b87bf62646257e
PavanPonnella <125259844+pavanponnella@users.noreply.github.com>
2025-01-29 16:42:30 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# Install Maven
-apt-get update && apt-get install -y maven
+
+# Install SDKMAN (used to install Maven)
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+# Install Maven using SDKMAN
+sdk install maven
+
 # Verify Maven installation
 mvn -version
+
 # Run the Maven build
 mvn clean install
