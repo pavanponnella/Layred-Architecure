b8c2097dc99634ae9932729ba47fb1938b26e5c8
PavanPonnella <125259844+pavanponnella@users.noreply.github.com>
2025-01-29 17:13:32 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Install SDKMAN (used to install Maven)
+# Install SDKMAN (used to install Java & Maven)
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-# Install Maven using SDKMAN
+# Install Java (Replace with the required version)
+sdk install java 17.0.9-tem
+
+# Set JAVA_HOME (Needed for Maven)
+export JAVA_HOME="$HOME/.sdkman/candidates/java/current"
+export PATH="$JAVA_HOME/bin:$PATH"
+
+# Verify Java installation
+java -version
+
+# Install Maven
 sdk install maven
 
 # Verify Maven installation
